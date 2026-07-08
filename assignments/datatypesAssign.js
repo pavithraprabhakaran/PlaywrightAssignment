2c951c1b7eb13e664ffe61e9fe567d32b16eb8a9
pavithraprabhakaran <31839329+pavithraprabhakaran@users.noreply.github.com>
2026-07-08 16:11:38 +0530

    Add files via upload
    JS Basic Assign1 (7 July)

---
+++ assignments/datatypesAssign.js
@@ -0,0 +1,24 @@
+//Firstname 
+var firstName ="Snhiv"
+console.log(firstName)
+console.log(typeof firstName)
+
+//Company name
+var companyName ="xyzcompany"
+console.log(companyName)
+console.log(typeof companyName)
+
+//Mobile number
+var mobileNumber=123456789
+console.log(mobileNumber)
+console.log(typeof mobileNumber)
+
+//IsAutomation
+var isAutomation=true
+console.log(isAutomation)
+console.log(typeof isAutomation)
+
+//hasPlaywright
+var hasPlaywright
+console.log(hasPlaywright)
+console.log(typeof hasPlaywright)
