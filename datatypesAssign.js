30796f50661ef38dad09c239d10b852173931247
Pavi <pavi@Pavis-MacBook-Air.local>
2026-07-09 11:30:31 +0530

    folder structure change

--- datatypesAssign.js
+++ datatypesAssign.js
@@ -4,21 +4,21 @@ console.log(firstName)
 console.log(typeof firstName)
 
 //Company name
-let companyName ="xyzcompany"
+var companyName ="xyzcompany"
 console.log(companyName)
 console.log(typeof companyName)
 
 //Mobile number
-const mobileNumber=123456789
+var mobileNumber=123456789
 console.log(mobileNumber)
 console.log(typeof mobileNumber)
 
 //IsAutomation
-let isAutomation=true
+var isAutomation=true
 console.log(isAutomation)
 console.log(typeof isAutomation)
 
 //hasPlaywright
-let hasPlaywright
+var hasPlaywright
 console.log(hasPlaywright)
 console.log(typeof hasPlaywright)
